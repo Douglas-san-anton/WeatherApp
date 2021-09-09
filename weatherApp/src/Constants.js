30d716e7d143cc5a6022adcc6d581eb002a0e940
Douglas San Antón <73114495+Douglas-san-anton@users.noreply.github.com>
2021-09-08 21:27:37 -0300

    Modified url

--- weatherApp/src/Constants.js
+++ weatherApp/src/Constants.js
@@ -1,4 +1,4 @@
-const BASE_URL= 'http://api.openweathermap.org/data/';
+const BASE_URL= 'https://api.openweathermap.org/data/';
 const BASE_NEXT= '2.5/weather?q='
 const API_KEY = '4ae2636d8dfbdc3044bede63951a019b';
 
